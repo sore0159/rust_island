48a9ff28b96035f8f51b6490e88b792e08b8c039
Mule <3.7mule@gmail.com>
2020-10-27 15:00:11 -0500

    making state generic, see if it works out

--- src/state.rs
+++ src/state.rs
@@ -1,25 +1,26 @@
-use crate::data::Game;
+// C is any UI output, D is game data
+pub struct StateData<C, D> {
+    pub data: D,
+    pub canvas: C,
+}
 
-pub trait State {
-    fn on_start(&mut self, g: &mut Game) -> Trans;
-    fn on_end(&mut self, g: &mut Game);
-    fn on_pause(&mut self, g: &mut Game);
-    fn on_resume(&mut self, g: &mut Game) -> Trans;
-    fn handle_event(&mut self, e: Event);
-    fn tic(&mut self) -> Trans;
+// E is event type provided by UI
+pub trait State<C, D, E> {
+    fn on_start(&mut self, d: &mut StateData<C, D>) -> Trans<C, D, E>;
+    fn on_end(&mut self, d: &mut StateData<C, D>);
+    fn on_pause(&mut self, d: &mut StateData<C, D>);
+    fn on_resume(&mut self, d: &mut StateData<C, D>) -> Trans<C, D, E>;
+    fn handle_event(&mut self, e: E);
+    fn tic(&mut self) -> Trans<C, D, E>;
     fn shadow_tic(&mut self);
 }
 
-pub enum Event {
-    Key,
-}
-
-pub enum Trans {
+pub enum Trans<C, D, E> {
     None,
     PopMe,
-    Push(Box<dyn State>),
-    PushMulti(Vec<Box<dyn State>>),
+    Push(Box<dyn State<C, D, E>>),
+    PushMulti(Vec<Box<dyn State<C, D, E>>>),
     PopAll,
-    Sequence(Vec<Trans>),
+    Sequence(Vec<Trans<C, D, E>>),
     Quit,
 }
